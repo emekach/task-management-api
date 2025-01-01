a65803708043269ad3c948a992d6481df82900f9
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-01 19:19:07 +0000

    refactor(structure):reorganize project folders restructured the project folder to follow a modular architecture. Introduced  as the root for core files

--- src/app.js
+++ src/app.js
@@ -6,29 +6,22 @@ const cors = require('cors');
 
 const globalErrorHandler = require('./middleware/errorHandler');
 
-// const whitelist = ['https://www.yoursite.com'];
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by cors'));
-//     }
-//   },
-//   optionsSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-
-app.use(cors());
-// Parse URL-encoded data
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
 // Parse JSON payloads with a size limit of 30MB
-app.use(express.json({ limit: '30mb' }));
+app.use(express.json({ limit: '30kb' }));
+
+// Parse URL-encoded data
+app.use(express.urlencoded({ extended: true, limit: '30kb' }));
 
 // serve static files
-app.use(express.static(path.join(__dirname, '/public')));
+//console.log(path.join(__dirname, '../public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // routes
 app.get('/', (req, res) => {
