b6fac7105caa29b98e1f746bec3b2664f74c56cb
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-01 13:37:41 +0000

    feat(db): implemented MondoDB connection - Added a function to connect to MongoDB using mongoose - utilised env variables for secure configuaration - Ensured proper error handling for connection issues2

---
+++ config/dbConn.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const DB = process.env.DATABASE_URI.replace(
+      '<db_password>',
+      process.env.DATABASE_PASSWORD,
+    );
+    await mongoose.connect(DB, {});
+    console.log('Database connected successfully');
+  } catch (err) {
+    console.error('Database connection failed:', err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
