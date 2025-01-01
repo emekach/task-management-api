f07174f21ce54edc12582e3ab84e14e2d2bc7933
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-01 17:54:46 +0000

    refactor(structure):reorganize project folders restructured the project folder to follow a modular architecture. Introduced  as the root for core files

---
+++ src/config/dbConn.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.DATABASE_URI);
+    const DB = process.env.DATABASE_URI.replace(
+      '<db_password>',
+      process.env.DATABASE_PASSWORD,
+    );
+    await mongoose.connect(DB, {});
+    console.log('Database connected successfully');
+  } catch (err) {
+    console.error('Database connection failed:', err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
