5d22173aba1c58487e088348367fce3ccb8d4f41
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-03 20:56:08 +0000

    feat: implemented login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('./../controller/authController');
 const router = express.Router();
 
 router.post('/auth/register', authController.createUser);
+router.post('/auth/login', authController.login);
 
 module.exports = router;
