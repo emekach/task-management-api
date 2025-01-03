0337009b240e23a8af7664b6b5ea9cb2ffce6f18
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-03 20:53:27 +0000

    feat(auth): Implemented login controller and functionality

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -14,3 +14,23 @@ exports.createUser = async (req, res) => {
 
   jwt.createSendToken(newUser, 201, req, res);
 };
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: 'field cannot be empty',
+    });
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return res.status(400).json({
+      message: 'incorrect details',
+    });
+  }
+
+  jwt.createSendToken(user, 200, req, res);
+};
