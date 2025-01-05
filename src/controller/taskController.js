debfd9872b580c1384b964507ec5c375334776c6
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-05 23:06:38 +0000

    feat(controller): add task controller

---
+++ src/controller/taskController.js
@@ -0,0 +1,5 @@
+exports.createTask = (req, res) => {};
+exports.getAllTasks = (req, res) => {};
+exports.getTask = (req, res) => {};
+exports.updateTask = (req, res) => {};
+exports.deleteTask = (req, res) => {};
