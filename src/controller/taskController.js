e778205b6750579791651321dab39cfbd4cfc3b5
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-06 00:04:59 +0000

    feat(controller): implement the create new task logic

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -1,4 +1,30 @@
-exports.createTask = (req, res) => {};
+const Task = require('./../models/TaskModel');
+
+exports.createTask = async (req, res) => {
+  const { title, description, dueDate, assignedTo, createdBy, tags } = req.body;
+  try {
+    const newTask = await Task.create({
+      title,
+      description,
+      dueDate,
+      assignedTo,
+      createdBy,
+      tags,
+    });
+
+    // console.log(newTask);
+
+    res.status(200).json({
+      message: 'suceess',
+      data: { newTask },
+    });
+  } catch (err) {
+    res.status(404).json({
+      msg: 'failed',
+      data: err.message,
+    });
+  }
+};
 exports.getAllTasks = (req, res) => {};
 exports.getTask = (req, res) => {};
 exports.updateTask = (req, res) => {};
