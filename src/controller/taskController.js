017908c29b3f9200a2766b5eea281ae88696277a
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-07 16:27:08 +0000

    feat: implement simple crud operation for task functionality

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -1,4 +1,5 @@
 const Task = require('./../models/TaskModel');
+const Task = require('./../models/TaskModel');
 
 exports.createTask = async (req, res) => {
   const { title, description, dueDate, assignedTo, createdBy, tags } = req.body;
@@ -25,7 +26,52 @@ exports.createTask = async (req, res) => {
     });
   }
 };
-exports.getAllTasks = (req, res) => {};
-exports.getTask = (req, res) => {};
-exports.updateTask = (req, res) => {};
-exports.deleteTask = (req, res) => {};
+exports.getAllTasks = async (req, res) => {
+  const data = await Task.findById(req.params.id);
+  if (!data) {
+    res.status(404).json({ message: 'no task found with that id' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data,
+    },
+  });
+};
+exports.getTask = async (req, res) => {
+  const data = await Task.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: data.length,
+    data: {
+      data,
+    },
+  });
+};
+exports.updateTask = async (req, res) => {
+  const data = await Task.findByIdAndUpdate(req.params.id);
+  if (!data) {
+    res.status(404).json({ message: 'no task found' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data,
+    },
+  });
+};
+exports.deleteTask = async (req, res) => {
+  const data = await Task.findByIdAndDelete(req.params.id);
+
+  if (!data) {
+    res.status(404).json({ message: 'no task found' });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
