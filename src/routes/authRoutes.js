6b594dffbdae5d6f155417b7193e75aaeade0caa
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-11 16:39:29 +0000

    feat: updated missing routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 
 router.post('/auth/register', authController.createUser);
 router.post('/auth/login', authController.login);
+router.route('/refresh-token').post(authController.refreshAccessToken);
 
+// secured route
+router.route('/change-password').post(protect, authController.updatePassword);
 router.route('/logout').post(protect, authController.logoutUser);
 
 module.exports = router;
